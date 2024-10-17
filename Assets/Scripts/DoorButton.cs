c140a4a60a8c4504976e7221b66abedff03048b7
fdezbarroso <f.fdezbarroso@gmail.com>
2024-10-17 18:57:22 +0200

    Add Door, DoorButton and Spheres

---
+++ Assets/Scripts/DoorButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DoorButton : MonoBehaviour
+{
+    //[SerializeField]
+    //private Door _door;
+    //[SerializeField]
+    //private Collider _trigger;
+
+    [SerializeField]
+    private UnityEvent _onTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _onTrigger?.Invoke();
+
+            // _door.Open();
+        }
+    }
+}
